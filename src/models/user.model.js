23d57b7167f3b0c5035804a31f7733c1bf6984d9
Nirjal Parbat <nirjalparbat@gmail.com>
2026-03-23 23:38:32 +0545

    Created register and login auth routes, usermodel and auth controller

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:  {
+        type: String,
+        unique: [true, "Username is Already Taken"],
+        required: true
+    },
+
+    email: {
+        type: String , 
+        unique: [true, "Account Already Exists With This Email Address"],
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const userModel = mongoose.model("users", userSchema)
+
+module.exports = userModel
